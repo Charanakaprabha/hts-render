ee928962e3df3ed2c7d9a8edb39335ee68ed22d9
Halftone Systems Developer <user@halftonesystems.com>
2026-02-23 12:09:39 +0530

    Fix proxy target for local development

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api-gemini/, ''),
       },
       '/api': {
-        target: 'http://127.0.0.1:3000',
+        target: 'http://localhost:3000',
         changeOrigin: true,
       },
     },
